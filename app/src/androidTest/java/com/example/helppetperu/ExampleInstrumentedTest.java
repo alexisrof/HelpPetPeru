40fa47010a5a8a0c0687063eaeff97ac1cfd1440
ARF <alexisrof@gmail.com>
2020-10-16 12:55:48 -0500

    Versión 0.0.1

--- app/src/androidTest/java/com/example/helppetperu/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/helppetperu/ExampleInstrumentedTest.java
@@ -1,9 +1,9 @@
 package com.example.helppetperu;
 
+import android.content.Context;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -13,8 +13,8 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        //Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        //Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         //assertEquals("com.example.helppetperu", appContext.getPackageName());
     }
